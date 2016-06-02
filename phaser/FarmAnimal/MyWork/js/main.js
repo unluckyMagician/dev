c2d4b8ee5056c6ab0413f3d76cdf287b4af10746
Ian Parker <IanPark@hpsvikings.org>
2016-06-02 14:37:45 -0400

    Input and Groups phaser tut

--- phaser/FarmAnimal/MyWork/js/main.js
+++ phaser/FarmAnimal/MyWork/js/main.js
@@ -5,26 +5,76 @@ var GameState = {
 	preload: function() {
 		this.game.load.image('background', 'assets/images/background.png');
 		this.game.load.image('chicken', 'assets/images/chicken.png');
-		this.game.load.image('horse', 'assets/images/horse.png');
 		this.game.load.image('pig', 'assets/images/pig.png');
+		this.game.load.image('horse', 'assets/images/horse.png');
 		this.game.load.image('sheep', 'assets/images/sheep3.png');
+		this.game.load.image('arrow', 'assets/images/arrow.png')
+
 	},
 
 	create: function() {
 
+		//Scaling
 		this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL
+
+		//Align game centered on screen
 		this.scale.pageAlignHorizontally = true;
 		this.scale.pageAlignVertically = true;
 
+		//background sprite
 		this.background = this.game.add.sprite(0, 0, 'background')
-		this.chicken = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'chicken');
-		this.chicken.anchor.setTo(0.5);
-		this.chicken.scale.setTo(1.5);
+		
+		var animalData = [
+			{key: 'chicken', text: 'CHICKEN'},
+			{key: 'horse', text: 'HORSE'},
+			{key: 'pig', text: 'PIG'},
+			{key: 'sheep', text: 'SHEEP'}
+
+		];
+
+		this.animals = this.game.add.group();
+
+		var self = this;
+		var animal;
+		animalData.forEach(function(element) {
+			animal = self.animals.create(self.game.world.centerX, self.game.world.centerY, element.key);
+			animal.customParams = {text: element.text};
+			animal.anchor.setTo(0.5);
+			animal.inputEnabled = true;
+
+		});
+
+		this.rightArrow = this.game.add.sprite(580, this.game.world.centerY, 'arrow');
+		this.rightArrow.anchor.setTo(0.5);
+		this.rightArrow.customParams = {direction: 1}
+
+		this.leftArrow = this.game.add.sprite(60, this.game.world.centerY, 'arrow');
+		this.leftArrow.anchor.setTo(0.5);
+		this.leftArrow.scale.x = -1;
+		this.leftArrow.customParams = {direction: 1}
+
+		this.leftArrow.inputEnabled = true;
+		this.leftArrow.input.pixelPerfectClick = true;
+		this.leftArrow.events.onInputDown.add(this.switchAnimal, this)
+
+		this.rightArrow.inputEnabled = true;
+		this.rightArrow.input.pixelPerfectClick = true;
+		this.rightArrow.events.onInputDown.add(this.switchAnimal, this)
+
+	
 
 	},
 
 	update: function() {
-		this.chicken.angle -= 1
+	
+	},
+
+	switchAnimal: function(sprite, event) {
+		console.log('move animal');
+	},
+
+	animateAnimal: function(sprite, event) {
+		console.log('animate animal');
 	}
 };
 
